9bb7bfdb8ef1ba44a384d9483505ec17e79403cd
Pablo Neira Ayuso <pablo@netfilter.org>
2025-04-22 21:52:44 +0200

    netfilter: nft_set_pipapo: clamp maximum map bucket size to INT_MAX
    commit b85e3367a5716ed3662a4fe266525190d2af76df upstream.
    
    Otherwise, it is possible to hit WARN_ON_ONCE in __kvmalloc_node_noprof()
    when resizing hashtable because __GFP_NOWARN is unset.
    
    Similar to:
    
      b541ba7d1f5a ("netfilter: conntrack: clamp maximum hashtable size to INT_MAX")
    
    Reviewed-by: Stefano Brivio <sbrivio@redhat.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    [Resolved contextual conflicts. Replaced use of rules_alloc with rules.]
    Signed-off-by: Aaron Ahmed <aarnahmd@amazon.com>

--- net/netfilter/nft_set_pipapo.c
+++ net/netfilter/nft_set_pipapo.c
@@ -693,6 +693,9 @@ static int pipapo_resize(struct nft_pipapo_field *f, int old_rules, int rules)
 	}
 
 mt:
+	if (rules > (INT_MAX / sizeof(*new_mt)))
+		return -ENOMEM;
+	
 	new_mt = kvmalloc(rules * sizeof(*new_mt), GFP_KERNEL);
 	if (!new_mt) {
 		kvfree(new_lt);
@@ -1390,6 +1393,9 @@ static struct nft_pipapo_match *pipapo_clone(struct nft_pipapo_match *old)
 		       src->bsize * sizeof(*dst->lt) *
 		       src->groups * NFT_PIPAPO_BUCKETS(src->bb));
 
+		if (src->rules > (INT_MAX / sizeof(*src->mt)))
+			goto out_mt;
+		
 		dst->mt = kvmalloc(src->rules * sizeof(*src->mt), GFP_KERNEL);
 		if (!dst->mt)
 			goto out_mt;
